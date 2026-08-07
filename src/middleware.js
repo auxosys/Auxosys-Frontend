d194b16e70e9c91742053b5df8f9118b17ec94ff
auxosys <auxosys@gmail.com>
2026-08-08 04:29:37 +0530

    fix: ignore site.webmanifest in middleware to fix 404 on verify subdomain

--- src/middleware.js
+++ src/middleware.js
@@ -19,5 +19,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\\\.svg|.*\\\\.png).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|site.webmanifest|.*\\\\.svg|.*\\\\.png|.*\\\\.jpg|.*\\\\.jpeg|.*\\\\.gif).*)'],
 };
