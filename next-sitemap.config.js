d12b70c9fa47f8fdb8d6af35e9f7be4508d0dc99
auxosys <auxosys@gmail.com>
2026-08-07 22:01:20 +0530

    fix: update sitemap siteUrl fallback to production domain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://auxosys.vercel.app',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://www.auxosys.com',
   generateRobotsTxt: true, // (optional)
   exclude: ['/404', '/500', '/admin', '/server-sitemap.xml'], // <= exclude here
   robotsTxtOptions: {
