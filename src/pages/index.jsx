eb7836b2f54398c63934af77dfe0e55c4a7f7b43
auxosys <auxosys@gmail.com>
2026-08-10 23:41:59 +0530

    style: Shift globe left to prevent clipping on mobile desktop view

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -295,8 +295,8 @@ export default function HomePage({ globalSeo }) {
           .home-legacy-hero .hero-right {
             grid-area: globe;
             transform: scale(0.9);
-            margin-left: -15%;
-            margin-right: -15%;
+            margin-left: -30%;
+            margin-right: 0;
             width: 130%;
             display: flex;
             justify-content: center;
