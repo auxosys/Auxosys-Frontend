f4b78959c58239ae1e467025733f88585dbe120c
auxosys <auxosys@gmail.com>
2026-08-10 23:40:19 +0530

    perf: Optimize webmanifest, drop legacy JS, fix contrast for a11y

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -47,7 +47,7 @@ export default function App({ Component, pageProps }) {
         <meta name="theme-color" content="#17262B" />
         
         {/* Web Manifest & Favicons */}
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" crossOrigin="anonymous" fetchPriority="low" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
